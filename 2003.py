1ead70e91409eff5a178912cd797e5daa4ff05a8
stack1245 <halleiqd1937@gmail.com>
2025-07-30 02:59:30 +0900

    2003

--- 2003.py
+++ 2003.py
@@ -2,13 +2,17 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
 count = 0
-for i in range(N):
-    current_sum = 0
-    for j in range(i, N):
-        current_sum += A[j]
-        if current_sum == M:
-            count += 1
-        if current_sum > M:
-            break
+left = 0
+current_sum = 0
+
+for right in range(N):
+    current_sum += A[right]
+
+    while current_sum > M:
+        current_sum -= A[left]
+        left += 1
+
+    if current_sum == M:
+        count += 1
 
 print(count)
